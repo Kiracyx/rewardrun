8ec5114440b432e1f464c83322869308c7aec2f1
Mykola Khomiakov <vkontemail4@gmail.com>
2025-10-12 20:32:31 +0200

    Create Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,1 +1,16 @@
+import Carousel from "./Carousel";
 
+export default function Header({ compact = false }: { compact?: boolean }) {
+  return (
+    <header className="header" id="header">
+      {!compact && (
+        <div className="header__bottom">
+          <span className="main__title">Choose your reward</span>
+          <div className="carousel-container">
+            <Carousel />
+          </div>
+        </div>
+      )}
+    </header>
+  );
+}
